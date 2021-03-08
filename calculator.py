01271e146611f653bbc92bf1919a773d4d20def1
The-Bronk <jesper.christoffersson@hotmail.com>
2021-03-08 23:44:27 +0100

    Skapade en fil

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Class som skapar datatypen miniräknkare
+ # Sends in user input and tests for 
+
+class:
+
+    def __init__(calculator, operator, term, quantity):
+        calculator.operator = operator
+        calculator.term = term
+        calculator.quantity =quantity
