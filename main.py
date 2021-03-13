ae98c508f5281b98a23df3dcf95af29ab7e74f01
The-Bronk <jesper.christoffersson@hotmail.com>
2021-03-13 23:56:51 +0100

    Har bantat koden rejält och gjort en välbehövd städning

--- main.py
+++ main.py
@@ -5,13 +5,11 @@
 import operator
 import random
 
-# TODO Skapa en funktion som tar emot operator och ger list_calculations & list_solution,(idag via create_dict),
-#  skapa även funktion för random i klassen
-
-# TODO Lägg till boolean som en eller flera operatorer
-
-
-# TODO ersätt string med f string
+# TODO Skapa en Klass av funktionen calculator
+# TODO Skapa funktion för random
+# TODO Skapa en funktion för operator --> Fråga Niklas, CodeWiz?
+# TODO Placera string_dict i funktionen inputtest för att kunna ställa prompts i funktionen
+# TODO What about negativa tal i calculator
 
 # Create dictionary for operators
 operator_dict = {
@@ -22,72 +20,40 @@ operator_dict = {
     # Logiska opperander i operator är funktioner, de kräver a  och b "^": operator.__ge__(a,b)
 }
 
-# print(operator_dict)
+string_dict = {
+    "term": "Vänligen ange ett tal inom intervallet 0 till 3",
+    "quantity": "Vänligen ange ett tal inom intervallet 1 till 15",
+    "operator": "Vänligen ange en operator"
+}
 
-
-# Skapar en funktion för ett konsekvent språkbruk
-def choose_a(string):
-    print("Vänligen ange en " + string)
-
-
-# Three while loops that creates user inputs and control of the inputs
-# TODO, convert whileloops to function eller Class? Förenkla koden på något vis, kanske påverkar lösning
-
-# lösning 1, funktion som tar emot user input som string och kontrollerar om de kan konverteras till int, skickar sedan konverterad data
-def inputtest(usrinput, interval):
-    returnvalue = None
+# Funktion som kontrollerar value error samt intervall från diverse user_inputs
+def inputtest(usrinput, errorprompt, högre, lägre): 
     while True:
         try:
             int(usrinput)
         except ValueError:
-            usrinput= input("Ange ett tal inom intervallet: " + interval)
+            usrinput= input(errorprompt)
             continue
+        usrinput = int(usrinput)
+        if usrinput > högre or usrinput < lägre:
+            usrinput= input(errorprompt)
         else:
             break
-    
-    return int(usrinput)
+    return usrinput
 
-
+# Fråga efter termens storlek
 term_input = input("Ange antal nollor du önskar öva med (0, 1, 2, eller 3)")
+term_ok = inputtest(term_input, string_dict["term"], 3, 0)
 
-# print(type(inputtest(term_input, "0, 1, 2, eller 3")))
-term_ok = inputtest(term_input, "0, 1, 2, eller 3")
-print(term_ok)
+# Fråga efter antal räkneoperationer 
+quantity_input = (input("Ange hur många räkneoperationer du önskar, max antal 15"))
+quantity_ok = inputtest(quantity_input, string_dict["quantity"], 15, 1)
 
 while True:
     operator_input = str(input("Ange en av följande operatorer: +, -, /, *,"))
     if operator_input not in operator_dict:
-        choose_a("operator")
+        print(string_dict["operator"])
         # continue
-    else:
-        break
-
-# TODO Önskar endast fånga endast när input inte är integer, debugga om fångar mer
-while True:
-    try:
-        term_input = int(input("Ange antal nollor du önskar öva med (0, 1, 2, eller 3)"))
-    except ValueError:
-        choose_a("tal inom intervallet (1)")
-        continue
-
-    if term_input > 3 or term_input < 0:
-        choose_a("tal inom intervallet (2)")
-    # Om terminput är en integer men integern överstiger önskat intervall
-    # TODO Bestäm datatyp på antal nollor och andra siffror som ska matas in, integer, float, etc
-    else:
-        break
-
-
-quantity_input = None
-
-while True:
-    try:
-        quantity_input = int(input("Ange hur många räkneoperationer du önskar, max antal 15"))
-    except ValueError:
-        choose_a("antal inom intervallet (1)")
-        continue
-    if quantity_input > 15 or quantity_input <= 0:
-        choose_a("antal inom intervallet (2)")
     else:
         break
 
@@ -95,13 +61,11 @@ while True:
 list_calculations = []
 list_solution = []
 
-# TODO ? ge möjlighet att välja bort  tal vilka leder till negativa tal?
-
-def create_dict(quantity, list1, list2):
+def calculator(quantity, list1, list2):
     # send in quantity_input and term_input return lists calculations and solutions
     for i in range(0, quantity):
-        term1 = random.randrange(10**term_input, 10**(term_input+1))
-        term2 = random.randrange(10**term_input, 10**(term_input + 1))
+        term1 = random.randrange(10**term_ok, 10**(term_ok+1))
+        term2 = random.randrange(10**term_ok, 10**(term_ok + 1))
         result = operator_dict[operator_input](term1, term2)
         # print(result)
         # print(result_add)
@@ -110,8 +74,8 @@ def create_dict(quantity, list1, list2):
         list1.append(calculation_operation)
         list2.append(calculation_solution)
 
-
-create_dict(quantity_input, list_calculations, list_solution)
+# Ändrade quantity_input till quantity_ok pga ny funktion, testa!
+create_dict(quantity_ok, list_calculations, list_solution)
 
 print(list_calculations)
 print(list_solution)
